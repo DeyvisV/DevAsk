e96216f4d864e9657f60546bb66281bba7bd46fc
Deyvis Valdez <deyvis.vh@gmail.com>
2016-06-09 17:22:44 -0500

    static files

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const server = express();
 // Config Swig
 server.engine('html', swig.renderFile);
 server.set('view engine', 'html');
-//server.set('views', __dirname + '/app/views');
 server.set('views', path.join(__dirname, 'app', 'views'));
+
+server.use(express.static('./public'));
 
 require('./app/controllers/home')(server);
 
